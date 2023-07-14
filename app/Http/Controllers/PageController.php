14d8854b12a210e1267d9b50e2372dde09553f4d
damlanureldemir <damlanureldemir@gmail.com>
2023-07-14 23:49:54 +0300

    düzenleme yapıldı

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+
+    public function products(){
+        return view('admin.products.products');
+    }
+
+    public function detail(){
+        return view('admin.products.product_detail');
+    }
+
+    public function cart(){
+        return view('admin.cart.cart');
+    }
+
+    public function contact(){
+        return view('frontend.pages.contact');
+    }
+
+    public function about(){
+        return view('frontend.pages.about');
+    }
+}
